162850bb70d9614fe55f919916bbc6098118c3b8
atari-monk <atari.monk1@gmail.com>
2023-12-28 22:29:34 +0100

    pomodoro 1

---
+++ js/primitives/point.js
@@ -0,0 +1,14 @@
+class Point {
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
+
+  draw(ctx, size = 18, color = 'black') {
+    const r = size / 2
+    ctx.beginPath()
+    ctx.fillStyle = color
+    ctx.arc(this.x, this.y, r, 0, Math.PI * 2)
+    ctx.fill()
+  }
+}
