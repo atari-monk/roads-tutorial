00546147451e57a9d278b78330f33a860b4b4c4d
atari-monk <atari.monk1@gmail.com>
2024-01-02 20:14:58 +0100

    start modern editor

---
+++ js/graphEditor.js
@@ -0,0 +1,60 @@
+class GraphEditor {
+  constructor(canvas, graph) {
+    this.canvas = canvas
+    this.graph = graph
+
+    this.ctx = this.canvas.getContext('2d')
+
+    this.selected = null
+    this.hovered = null
+
+    this.#addEventListeners()
+  }
+
+  #addEventListeners() {
+    this.canvas.addEventListener('mousedown', (evt) => {
+      //right click
+      if (evt.button === 2) {
+        if (this.hovered) {
+          this.#removePoint(this.hovered)
+        }
+      }
+      //left click
+      if (evt.button === 0) {
+        const mouse = new Point(evt.offsetX, evt.offsetY)
+        if (this.hovered) {
+          this.selected = this.hovered
+          return
+        }
+        this.graph.addPoint(mouse)
+        this.selected = mouse
+        this.hovered = mouse
+      }
+    })
+    this.canvas.addEventListener('mousemove', (evt) => {
+      const mouse = new Point(evt.offsetX, evt.offsetY)
+      this.hovered = getNearestPoint(mouse, this.graph.points, 10)
+    })
+    this.canvas.addEventListener('contextmenu', (evt) => {
+      evt.preventDefault()
+    })
+  }
+
+  #removePoint(point) {
+    this.graph.removePoint(point)
+    this.hovered = null
+    if (this.selected === point) {
+      this.selected = null
+    }
+  }
+
+  display() {
+    this.graph.draw(this.ctx)
+    if (this.hovered) {
+      this.hovered.draw(this.ctx, { fill: true })
+    }
+    if (this.selected) {
+      this.selected.draw(this.ctx, { outline: true })
+    }
+  }
+}
